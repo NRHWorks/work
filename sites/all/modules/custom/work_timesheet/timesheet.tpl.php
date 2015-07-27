dd58114bbe79e7c6df57b23975cdd20ae02a1a15
mjohns660 <matt@nrhworks.com>
2015-07-26 22:12:21 -0400

    Timesheet updates
    Start date and End Date form

--- sites/all/modules/custom/work_timesheet/timesheet.tpl.php
+++ sites/all/modules/custom/work_timesheet/timesheet.tpl.php
@@ -1,4 +1,17 @@
+   <form action="/timesheet" method="get">
+   <div>
+       <label for="name">Start date:</label>
+       <input type="text" id="start_date" />
+   </div>
+   <div>
+      <label for="name">End date:</label>
+      <input type="text" id="end_date" />
+   </div>
+   <div class="button">
+       <button type="submit">Generate Report</button>
+   </div>
+   </form>
 <?php
-
-   $table = array();
-   print $table; exit;
+   $parameters = drupal_get_query_parameters();
+   print $parameters; 
+   //print $table; 
