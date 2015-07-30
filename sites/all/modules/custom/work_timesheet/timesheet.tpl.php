ed6793c37b4d3e155e368e820a27a4a5c7709caa
mjohns660 <matt@nrhworks.com>
2015-07-30 17:15:35 -0400

    dynamic query updates

--- sites/all/modules/custom/work_timesheet/timesheet.tpl.php
+++ sites/all/modules/custom/work_timesheet/timesheet.tpl.php
@@ -1,11 +1,11 @@
    <form action="/timesheet" method="get">
    <div>
        <label for="name">Start date:</label>
-       <input type="text" id="start_date" />
+       <input type="text" id="start_date" name="start_date"/>
    </div>
    <div>
       <label for="name">End date:</label>
-      <input type="text" id="end_date" />
+      <input type="text" id="end_date" name="end_date"/>
    </div>
    <div class="button">
        <input type="submit" value="Generate Report" />
