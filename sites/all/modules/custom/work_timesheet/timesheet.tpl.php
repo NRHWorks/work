f92f0e07798f765aafe9cd2ceb91a547200cf971
mjohns660 <matt@nrhworks.com>
2015-08-07 18:33:32 -0400

    sort changes

--- sites/all/modules/custom/work_timesheet/timesheet.tpl.php
+++ sites/all/modules/custom/work_timesheet/timesheet.tpl.php
@@ -7,11 +7,18 @@
       <label for="name">End date:</label>
       <input type="text" id="end_date" name="end_date"/>
    </div>
+   <div>
+      <input type="radio" name="group_by" value="any">Any<br>
+      <input type="radio" name="group_by" value="client">Client<br>
+      <input type="radio" name="group_by" value="developer">Developer<br>
+      <input type="radio" name="group_by" value="project">Project
+   </div>
    <div class="button">
        <input type="submit" value="Generate Report" />
    </div>
    </form>
 <?php
 foreach($table as $t) {
+   header('Content-Type: text/plain');
    print $t; 
  }
