c854f03ffb08210ca411c9cb9f4f790a55f8c3e9
mjohns660 <matt@nrhworks.com>
2015-08-07 19:45:23 -0400

    Timesheet fixes stable

--- sites/all/modules/custom/work_timesheet/timesheet.tpl.php
+++ sites/all/modules/custom/work_timesheet/timesheet.tpl.php
@@ -1,5 +1,6 @@
    <form action="/timesheet" method="get">
-   <div>
+    <div style="float:left">
+      <div>
        <label for="name">Start date:</label>
        <input type="text" id="start_date" name="start_date"/>
    </div>
@@ -7,16 +8,18 @@
       <label for="name">End date:</label>
       <input type="text" id="end_date" name="end_date"/>
    </div>
-   <div>
-      <input type="radio" name="group_by" value="any">Any<br>
-      <input type="radio" name="group_by" value="client">Client<br>
-      <input type="radio" name="group_by" value="developer">Developer<br>
-      <input type="radio" name="group_by" value="project">Project
-   </div>
-   <div class="button">
+   <div class="button" style="margin-top: 10px;">
        <input type="submit" value="Generate Report" />
    </div>
+ </div>
+    <div style="float:left; margin: 20px 0px 0px 20px;">
+      <input type="radio" name="group_by" value="any"> Any<br>
+      <input type="radio" name="group_by" value="client"> Client<br>
+      <input type="radio" name="group_by" value="developer"> Developer<br>
+      <input type="radio" name="group_by" value="project"> Project
+   </div>
    </form>
+   <div style="clear:both;"></div>
 <?php
 foreach($table as $t) {
    header('Content-Type: text/plain');
