85021430f940b03fd724142c42879fa3596e5573
Development Server <dev-server@nrhworks.com>
2015-08-15 01:45:59 -0500

    adding todo module

---
+++ sites/all/modules/custom/work_todo/templates/todo.tpl.php
@@ -0,0 +1,78 @@
+<style type="text/css">
+  .todo-wrapper {
+    clear: both;
+    border-bottom: 1px solid #CCC;
+    overflow: auto;
+    padding: 10px 0px;
+  }
+
+  .todo-wrapper:last-child {
+    border-bottom: 0px;
+  }
+
+  .todo-list-item {
+    float: left;
+    font-size: 18px;
+  }
+  
+  .todo-list-item-sm {
+    float: left;
+    font-size: 14px;
+  }
+
+  .todo-actions {
+    float: right;
+  }
+</style>
+
+<div class="todo-group">
+<?php foreach ($data as $t) : ?>
+  <?php if (($t->field_todo_status[LANGUAGE_NONE][0]['value'] == 0) && ($t->field_date[LANGUAGE_NONE][0]['value'] <= date('Y-m-d 24'))) : ?> 
+    
+    <div class="todo-wrapper">
+      <div class="todo-list-item">
+        <?php print $t->title; ?>
+      </div>
+      <div class="todo-actions">
+        <a href="/todo/done/<?php print $t->nid; ?>">Done</a> | <a href="/todo/do-later/<?php print $t->nid; ?>">Do Later</a> | <a href="/todo/do-tomorrow/<?php print $t->nid; ?>">Do Tomorrow</a>
+      </div>
+    </div>
+
+  <?php endif; ?>
+<?php endforeach; ?>
+</div>
+
+<br /><br />
+<h1>To Do: Tomorrow</h1>
+
+<div class="todo-group">
+<?php foreach ($data as $t) : ?>
+  <?php if (($t->field_todo_status[LANGUAGE_NONE][0]['value'] == 0) && ($t->field_date[LANGUAGE_NONE][0]['value'] > date('Y-m-d 24'))) : ?> 
+  
+    <div class="todo-wrapper">
+      <div class="todo-list-item-sm">
+        <?php print $t->title; ?>
+      </div>
+      <div class="todo-actions">
+        <a href="/todo/do-today/<?php print $t->nid; ?>">Do Today</a>
+      </div>
+    </div>
+
+  <?php endif; ?>
+<?php endforeach; ?>
+</div>
+
+<br /><br />
+<h1>To Do: Done</h1>
+
+<div class="todo-group">
+<?php foreach ($data_done as $t) : ?>
+  <?php if ($t->field_todo_status[LANGUAGE_NONE][0]['value'] == 1) : ?> 
+    <div class="todo-wrapper">
+      <div class="todo-list-item-sm">
+        <strike><?php print $t->title; ?></strike>
+      </div>
+    </div>
+  <?php endif; ?>
+<?php endforeach; ?>
+</div>
