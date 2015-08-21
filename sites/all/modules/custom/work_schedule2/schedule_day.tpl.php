5d4471da961f53d3f0c2b534b2fa51269af89f86
mjohns660 <matt@nrhworks.com>
2015-08-21 10:12:42 -0400

    Schedule Table updates

--- sites/all/modules/custom/work_schedule2/schedule_day.tpl.php
+++ sites/all/modules/custom/work_schedule2/schedule_day.tpl.php
@@ -1,3 +1,11 @@
+<form action="/schedule/day" method="get">
+    <div style="float:left">
+      <div>
+       <label for="name">Date:</label>
+       <input type="text" id="date" name="date"/>
+   	   </div>
+   <div>
+
 <?php
 //print 'here'; exit;
 foreach($table as $t) {
