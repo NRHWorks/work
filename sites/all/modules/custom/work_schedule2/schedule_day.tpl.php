21fe21b2a60e8f0a0e6a75123a042944cdf5823b
mjohns660 <matt@nrhworks.com>
2015-08-21 18:49:52 -0400

    Schedule updates

--- sites/all/modules/custom/work_schedule2/schedule_day.tpl.php
+++ sites/all/modules/custom/work_schedule2/schedule_day.tpl.php
@@ -1,10 +1,12 @@
 <form action="/schedule/day" method="get">
-    <div style="float:left">
-      <div>
-       <label for="name">Date:</label>
-       <input type="text" id="date" name="date"/>
-   	   </div>
-   <div>
+   	<div>
+      	<label for="name">Date:</label>
+      	<input type="text" id="date" name="date"/>
+   </div>
+   <div class="button" style="margin-top: 10px;">
+       <input type="submit" value="Submit" />
+   </div>
+</form>
 
 <?php
 //print 'here'; exit;
