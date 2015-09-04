b3bf9b430e2a03c1550b3beb180a465ae103a474
mjohns660 <matt@nrhworks.com>
2015-09-04 12:20:08 -0400

    Schedule styling fixes

--- sites/all/modules/custom/work_schedule2/schedule_day.tpl.php
+++ sites/all/modules/custom/work_schedule2/schedule_day.tpl.php
@@ -1,4 +1,8 @@
-<?php print theme('schedule_menu'); ?>
+<?php
+global $t;
+$t = $table;
+$date = $t[0];
+print theme('schedule_menu'); ?>
 
 <form action="/schedule/day" method="get">
     <div>
@@ -9,9 +13,25 @@
        <input type="submit" value="Submit" />
    </div>
 </form>
+<div>
+<h1>
+  <div style = "float: left; width: 50px;">
+    <a href="/schedule/day/<?php echo date('Y-m-d',strtotime('-1 days', strtotime($date)));?>" ><<</a>
+  </div>
+  <div style = "float: left; margin-left: 300px;"> 
+    <?php echo date('l, M j, Y', strtotime($date)); ?>
+  </div>
+  <div style = "float: right; width:50px">
+    <a href="/schedule/day/<?php echo date('Y-m-d',strtotime('+1 days', strtotime($date)));?>" >>></a>
+  </div>
+</h1>
+<br style="clear: left;" />
+</div>
 
 <?php
-foreach($table as $t) {
+global $t;
+$data = $t[1];
+foreach($data as $d) {
   header('Content-Type: text/plain');
-  print $t;
+  print $d;
 }
