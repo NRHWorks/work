df06c41b53654bc1294861adcc2586dd12b664d0
Ming-ching Chiu <percle@gmail.com>
2016-03-02 21:47:28 -0600

    modify field-assigned-to template and work_todo.module

--- sites/all/modules/custom/work_theme/templates/field--field-assigned-to.tpl.php
+++ sites/all/modules/custom/work_theme/templates/field--field-assigned-to.tpl.php
@@ -5,7 +5,10 @@
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
       <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
-        <a href="#" onclick="jQuery('#node-assign-to-div').toggle(); return false;"> <span id="node-assigned-to"><?php print $item['#title']; ?></span> </a>
+<?php /* ?>
+<a href="#" onclick="jQuery('#node-assign-to-div').toggle(); return false;"> <span id="node-assigned-to"><?php print $item['#title']; ?></span> </a>
+<?php */ ?>
+        <a href="#" onclick="return false;"> <span id="node-assigned-to"><?php print $item['#title']; ?></span> </a>
       </div>
     <?php endforeach; ?>
   </div>
